4ad522be2129dea14200bc324c75fa2f15d4f459
Lucas Börjesson <lucas.ri.borjesson@gmail.com>
2024-01-04 21:08:51 +0100

    Better entryfile.sh, better kustomization

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,25 @@
 #!/usr/bin/env sh
 
-echo $GPG_SECRET | base64 -d | gpg --armor --import
+if [ "$LOG_LEVEL" == "debug" ]; then
+	echo $GPG_SECRET | base64 -d | gpg --armor --import
+else
+	echo $GPG_SECRET | base64 -d | gpg --armor --import &> /dev/null
+fi
 
 mkdir -p /home/root/.config/gopass
 
 git config --global user.email "$GIT_USER_EMAIL"
 git config --global user.name "$GIT_USER_NAME"
 
-echo "Cloning secrets repo"
-git clone $SECRETS_REPO_URL /rundir/secrets
-
 cat << EOF > /home/root/.config/gopass/config
 [mounts]
 	path = /rundir/secrets
 EOF
 
-PASSWORD_STORE_DIR=/rundir/secrets gopass init $GPG_KEY_ID
+if [ "$LOG_LEVEL" == "debug" ]; then
+	gopass clone --check-keys=false $SECRETS_REPO_URL
+else
+	gopass clone --check-keys=false $SECRETS_REPO_URL &> /dev/null
+fi
 
 ./gopass-external-secrets
